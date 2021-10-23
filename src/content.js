9c97b686627fcb2d670fc0511286f34c789210d6
Tatsujin Chin <c910335@gmail.com>
2021-10-23 19:53:25 +0800

    improve login
    check login page
    try login when update account

--- src/content.js
+++ src/content.js
@@ -1,23 +1,44 @@
+function isLoginPage() {
+  let t = document.querySelector('[data-row="20"] .q7.b0');
+  return t && t.textContent == '請輸入代號，或以 guest 參觀，或以 new 註冊: ';
+}
+
+function login(id, password) {
+  let t = document.getElementById('t');
+  t.value = id;
+  t.dispatchEvent(new Event('input'));
+  t.dispatchEvent(new KeyboardEvent('keydown', {key: 'Enter', keyCode: 13, which: 13, bubbles: true}));
+  t.value = password;
+  t.dispatchEvent(new Event('input'));
+  t.dispatchEvent(new KeyboardEvent('keydown', {key: 'Enter', keyCode: 13, which: 13, bubbles: true}));
+}
+
+function start() {
+  let loginInterval = setInterval(() => {
+    if (isLoginPage()) {
+      clearInterval(loginInterval);
+      navigator.credentials.get({password: true}).then(cred => {
+        if (cred && cred.id && cred.password)
+          login(cred.id, cred.password);
+      })
+    }
+  }, 60);
+
+  setTimeout(() => {
+    clearInterval(loginInterval);
+  }, 5000);
+}
+
 chrome.runtime.onMessage.addListener((msg, _, sendResponse) => {
   if (msg) {
     if (msg.id && msg.password) {
       navigator.credentials.store(new PasswordCredential(msg));
       sendResponse('已更新');
+      if (isLoginPage())
+        login(msg.id, msg.password);
     } else if (msg == 'ping')
       sendResponse('pong');
   }
 });
 
-setTimeout(() => {
-  navigator.credentials.get({password: true}).then(cred => {
-    if (cred && cred.id && cred.password) {
-      let t = document.getElementById('t');
-      t.value = cred.id;
-      t.dispatchEvent(new Event('input'));
-      t.dispatchEvent(new KeyboardEvent('keydown', {key: 'Enter', keyCode: 13, which: 13, bubbles: true}));
-      t.value = cred.password;
-      t.dispatchEvent(new Event('input'));
-      t.dispatchEvent(new KeyboardEvent('keydown', {key: 'Enter', keyCode: 13, which: 13, bubbles: true}));
-    }
-  })
-}, 500);
+start();
